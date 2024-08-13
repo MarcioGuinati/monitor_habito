4cf8a75c18ac7894a25588e33c83ca56fb33de7f
biancamacedodev <biancamcedo97@gmail.com>
2024-08-13 17:06:30 -0300

    003-TelaEventos-componenteButtonSalvar

---
+++ my-app/components/ButtonSave/ButtonRegister.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet, GestureResponderEvent } from 'react-native';
+
+interface ButtonProps {
+  onPress?: (event: GestureResponderEvent) => void;
+}
+
+const ButtonSave: React.FC<ButtonProps> = ({ onPress }) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>Salvar</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    marginTop: 30,
+    backgroundColor: 'orange',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+    maxWidth: '50%'
+  },
+  text: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default ButtonSave;
